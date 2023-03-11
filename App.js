771dab29b3e0880f521d776b4eb9c108b69f7a25
bast38900 <bast38900@edu.ucl.dk>
2023-03-11 16:33:23 +0100

    addContext

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './assets/src/screen/IndexScreen';
+import { BlogProvider } from './assets/src/context/BlogContext';
 
 const navigator = createStackNavigator({
   Index: IndexScreen,
@@ -15,5 +16,7 @@ const navigator = createStackNavigator({
 const App = createAppContainer(navigator);
 
 export default () => {
-  return <App />;
+  return <BlogProvider>
+        <App />    
+    </BlogProvider>
 };
