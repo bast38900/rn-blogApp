386de9864b401f0fef8004628688e9a5e5f12a18
bast38900 <bast38900@edu.ucl.dk>
2023-03-13 11:22:28 +0100

    ..

--- assets/src/context/BlogContext.js
+++ assets/src/context/BlogContext.js
@@ -1,22 +1,29 @@
 // Adding context to the app, to move data around the app.
 
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 const BlogContext = React.createContext();
 
-export const BlogProvider = ({ children }) => {
-    // state variable for blog posts
-    const [blogPosts, setBlogPosts] = useState([]);
+// Adding Reducer to change state, and implement CRUD for blog post
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case "add_blogpost":
+            return [...state, { title: `Blog Post #${state.length + 1}` }];
+        default:
+            return state;
+    }
+};
 
-    // Create helperfunction to add a blog post, and update the state.
+export const BlogProvider = ({ children }) => {
+    // State variable and rerendering for blog posts, deafult is empty array.
+    const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
+    // Helper function to add a blog post
     const addBlogPost = () => {
-        setBlogPosts([
-            ...blogPosts, 
-            { title: `Blog Post #${blogPosts.length + 1}` }
-        ]);
+        dispatch({ type: "add_blogpost" });
     };
     
-    // For rendering when state changes, and getting the blog posts list.
+    // For rendering the whole app when state changes, and getting the blog posts list.
     return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
         {children}
     </BlogContext.Provider>
