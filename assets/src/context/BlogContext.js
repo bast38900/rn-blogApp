b30e9de71bde79cc55758451c3dc9bc13905e242
bast38900 <bast38900@edu.ucl.dk>
2023-03-15 09:08:49 +0100

    prettier enabled

--- assets/src/context/BlogContext.js
+++ assets/src/context/BlogContext.js
@@ -2,27 +2,33 @@
     Adding context to the app, to move data around the app. 
 */
 
-import createDataContext from './createDataContext';
+import createDataContext from "./createDataContext";
 
 // Adding Reducer to change state, and implement CRUD for blog post
 const blogReducer = (state, action) => {
-    switch (action.type) {
-        case "add_blogpost":
-            return [...state, { id: Math.floor(Math.random() * 99999), title: `Blog Post #${state.length + 1}` }];
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "add_blogpost":
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: `Blog Post #${state.length + 1}`,
+        },
+      ];
+    default:
+      return state;
+  }
 };
 
 // Helper function to add a blog post
-const addBlogPost = dispatch => {
-    return () => {
-        dispatch({ type: "add_blogpost" });
-    };
+const addBlogPost = (dispatch) => {
+  return () => {
+    dispatch({ type: "add_blogpost" });
+  };
 };
 
 export const { Context, Provider } = createDataContext(
-    blogReducer,
-    { addBlogPost },
-    []
+  blogReducer,
+  { addBlogPost },
+  []
 );
