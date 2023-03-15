d49c0516e1797cb1af278bf5864547fc82389cdd
bast38900 <bast38900@edu.ucl.dk>
2023-03-15 09:10:27 +0100

    ..

--- assets/src/context/createDataContext.js
+++ assets/src/context/createDataContext.js
@@ -2,30 +2,30 @@
     Automating Context Creation, via reusable function.
 */
 
-import React, { useReducer } from 'react';
+import React, { useReducer } from "react";
 
 export default (reducer, actions, initialState) => {
-    // Calling the context creator function
-    const Context = React.createContext();
+  // Calling the context creator function
+  const Context = React.createContext();
 
-    // Setting up the provider
-    const Provider = ({ children }) => {
-        // State variable and rerendering components on state change, and setting initial state.
-        const [state, dispatch] = useReducer(reducer, initialState);
+  // Setting up the provider
+  const Provider = ({ children }) => {
+    // State variable and rerendering components on state change, and setting initial state.
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-        // Function to handle actions with dispatch
-        const boundActions = {};
-        for (let key in actions) {
-          boundActions[key] = actions[key](dispatch);
-        }
+    // Function to handle actions with dispatch
+    const boundActions = {};
+    for (let key in actions) {
+      boundActions[key] = actions[key](dispatch);
+    }
 
-        // For rendering the whole app when state changes.
-        return (
-            <Context.Provider value={{ state, ...boundActions }}>
-              {children}
-            </Context.Provider>
-        );
-    };
+    // For rendering the whole app when state changes.
+    return (
+      <Context.Provider value={{ state, ...boundActions }}>
+        {children}
+      </Context.Provider>
+    );
+  };
 
-    return { Context, Provider };
+  return { Context, Provider };
 };
--- assets/src/screen/IndexScreen.js
+++ assets/src/screen/IndexScreen.js
@@ -3,47 +3,49 @@
 */
 
 import React, { useContext } from "react";
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import { Context } from "../context/BlogContext";
-import { Feather } from '@expo/vector-icons'; 
+import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = () => {
-    // Getting data from blog context, and passing it to the Flatlist component.
-    const { state, addBlogPost } = useContext(Context);
+  // Getting data from blog context, and passing it to the Flatlist component.
+  const { state, addBlogPost } = useContext(Context);
 
-    return (
-        <>
-            <Button title="Add Post" onPress={addBlogPost} />
-            <FlatList 
-                data={state}
-                keyExtractor={(blogPosts) => blogPosts.title}
-                renderItem={({ item }) => {
-                    return <View style={styles.row}>
-                        <Text style={styles.title}>{item.title}</Text>
-                        <Feather style={styles.icon} name="trash-2" />
-                    </View>
-                }}
-            />
-        </>
-    )
+  return (
+    <>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={(blogPosts) => blogPosts.title}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather style={styles.icon} name="trash-2" />
+            </View>
+          );
+        }}
+      />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-    row: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: 10,
-        backgroundColor: "white",
-        borderColor: "gray",
-        borderTopWidth: 1,
-    },
-    title: {
-        fontSize: 20,
-    },
-    icon: {
-        size: 20,
-    }
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "white",
+    borderColor: "gray",
+    borderTopWidth: 1,
+  },
+  title: {
+    fontSize: 20,
+  },
+  icon: {
+    size: 20,
+  },
 });
 
 export default IndexScreen;
